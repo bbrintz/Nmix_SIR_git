5bf5fc2eeaef182cbb3891f9482331dc12977bd3
BJB2088 <ben.brintz@hsc.utah.edu>
2024-06-15 10:27:30 -0600

    spatial beta model

---
+++ bin_logit_spatial.R
@@ -0,0 +1,233 @@
+library(tidyverse)
+library(cmdstanr)
+library(bayesplot)
+library(posterior)
+library(MASS)
+#set.seed(123)
+#beta <- 1.05
+#TT <- 100
+N_C <- 10 
+set.seed(31)
+
+# Spatial parameters
+# Parameters
+sigma <- .05 # Standard deviation of noise
+rho <- 1.2  # Spatial range parameter
+decay_rate_space <- 2 # Spatial decay rate
+
+DiffsMat=expand.grid(1:N_C, 1:N_C) #%>% mutate(dist=abs(Var1-Var2))
+DiffsMat %>% mutate(dist=abs(Var1-Var2)) -> D
+
+D <- matrix(D$dist,nrow=N_C)
+Sigma <- sigma^2 * exp(-D / rho)
+log_beta_diag=mvrnorm(1, rep(-.1, N_C), Sigma);log_beta_diag
+log_beta = matrix(0, N_C, N_C)
+diag(log_beta)=log_beta_diag
+
+for (i in 1:(N_C-1)) {
+  for (j in (i+1):(N_C)) {
+    distance_from_center <- abs(i - j)
+    log_beta[i, j] <- log_beta[i, i] - decay_rate_space * distance_from_center
+    log_beta[j, i] <- log_beta[i, j]
+  }
+}
+beta=exp(log_beta)
+
+gamma <- runif(N_C, min = 0.7, max = 0.8)
+TT <- 30
+pop_size <- 1e3 * sample(1:10,N_C,TRUE)
+I_0 <- sample(10:20,N_C,TRUE)
+S_0 <- pop_size - I_0
+ii <- R <- S <- I <- matrix(NA_real_,TT, N_C)
+p_detect <- 0.6
+S[1,] <- S_0
+I[1,] <- I_0
+R[1,] <- 0 
+#ii[1,] <- rbinom(N_C, I_0, p_detect)
+#for (t in 2:TT) {
+#  for (ct in  1:N_C) {
+    #if (t > 2)
+    #  prev_t <- colSums(I[1:(t-1),]) - colSums(R[1:(t-1),])
+    #else
+    #  prev_t <- I[1,] - R[1,]
+#    I[t,ct] <- rbinom(1, S[t-1,ct], 1 - exp(- sum(beta[ct,] * I[t-1,]/ pop_size)))
+#    S[t,ct] <- S[t - 1,ct] - I[t,ct]
+    #R[t,ct] <- rbinom(1, prev_t[ct], gamma[ct])
+    #ii[t,ct] <- rbinom(1, prev_t[ct] + I[t,ct] - R[t,ct], p_detect)
+#  }
+#}
+  for (t in 1:(TT-1)){
+    SIstore=rep(0,N_C)
+    for (ct in  1:N_C) {
+    SI=rbinom(N_C,S[t,],1-exp(- (beta[ct,] * I[t,]/ pop_size)))
+    SIstore=SIstore+SI
+    IR=rbinom(1,I[t,ct],gamma[ct])
+    I[t+1,ct]=I[t,ct]+sum(SI)-IR
+    #S[t+1,]=S[t,]-SI
+    R[t+1,ct]=R[t,ct]+IR
+  }
+  S[t+1,]=S[t,]-SIstore
+  }
+
+ii=matrix(rbinom(N_C*TT,I,p_detect),nrow=TT)
+
+matplot(ii, type="l")
+
+tt <- cmdstan_model("stoch_beta_spatial.stan")
+head((apply(I,2,cumsum) - apply(R,2,cumsum)))
+tail((apply(I,2,cumsum) - apply(R,2,cumsum)))
+(pop_size - S[TT,]) / pop_size
+
+dat <- 
+  list(
+    ii = ii,
+    TT = TT,
+    N_C = N_C,
+    pop_size = pop_size,
+    D=D
+  )
+
+fit <- tt$sample(data = dat, chains = 4,
+                 adapt_delta = 0.95,
+                 max_treedepth = 10,
+                 init = \() {list(u_t_logit_eta = matrix(qlogis(rbeta(TT*N_C, 1, 9)), TT, N_C),
+                                  w_t_logit_eta = matrix(qlogis(rbeta(TT*N_C, 1, 9)), TT, N_C),
+                                  p = rbeta(1, 4, 4),
+                                  log_beta_diag = rnorm(N_C,0,.001),
+                                  #b_od = rgamma(N_C-2, 4, 4),
+                                  #b_self = rgamma(N_C, 4, 4),
+                                  i0 = rbeta(N_C, 0.01*50, 0.99*50),
+                                  gamma = rbeta(N_C, 0.7 * 6, 0.3 * 6))},
+                 iter_warmup = 1000,
+                 iter_sampling = 1000, parallel_chains = 4,
+                 step_size = 1.5e-3)
+
+
+lims <- hist(fit$draws("p"),plot=FALSE)
+ymax <- lims$density |> max()
+hist(rbeta(1e6,0.75 * 5, 0.25 * 5),freq=FALSE,breaks=100, border = NA,
+     main = "Prior (grey) vs. posterior (red) for prob of detection",
+     xlab = "p", ylim = c(0,ymax+1))
+hist(fit$draws("p"),freq=FALSE,col=rgb(1,0,0,0.5),add=TRUE,border = NA)
+abline(v = 0.6,col="black")
+
+lims <- hist(fit$draws("beta[1,1]"),plot=FALSE)
+ymax <- lims$density |> max()
+hist(rgamma(1e6,4, 4),freq=FALSE,ylim=c(0,ymax+1),breaks=100, border = NA,
+     main = "Prior (grey) vs. posterior (red) for intra-county rate of infection in 1st cty",
+     xlab = bquote(beta[1,1]))
+hist(fit$draws("beta[1,1]"),freq=FALSE,col=rgb(1,0,0,0.5),add=TRUE,border = NA)
+abline(v = beta[1,1],col="black")
+
+lims <- hist(fit$draws("b[1,2]"),plot=FALSE)
+ymax <- lims$density |> max()
+hist(rgamma(1e6,4, 4),freq=FALSE,ylim=c(0,ymax+1),breaks=100, border = NA,
+     main = "Prior (grey) vs. posterior (red) for inter-county rate of infection for NN",
+     xlab = bquote(beta[ij]~", "~abs(i - j) == 1))
+hist(fit$draws("beta[1,2]"),freq=FALSE,col=rgb(1,0,0,0.5),add=TRUE,border = NA)
+abline(v = beta[1,2],col="black")
+
+lims <- hist(fit$draws("b[1,3]"),plot=FALSE)
+ymax <- lims$density |> max()
+hist(rgamma(1e6,4, 4),freq=FALSE,ylim=c(0,ymax+1),breaks=100, border = NA,
+     main = "Prior (grey) vs. posterior (red) for inter-county rate of infection for 2NN",
+     xlab = bquote(beta[ij]~", "~abs(i - j) == 2))
+hist(fit$draws("b[1,3]"),freq=FALSE,col=rgb(1,0,0,0.5),add=TRUE,border = NA)
+abline(v = beta[1,3],col="black")
+
+lims <- hist(-log(1-fit$draws("gamma[1]")),plot=FALSE)
+ymax <- lims$density |> max()
+hist(-log(1-rbeta(1e6,0.8 * 15, 0.2 * 15)),freq=FALSE,ylim=c(0,ymax+1), border = NA,breaks=100,
+     main = "Prior (grey) vs. posterior (red) for rate of recovery cty 1",
+     xlab = bquote(-log(1-gamma[1])))
+hist(-log(1-fit$draws("gamma[1]")),freq=FALSE,col=rgb(1,0,0,0.5),add=TRUE,border = NA)
+abline(v = -log(1-gamma[1]),col="black")
+
+lims <- hist(-log(1-fit$draws("gamma[2]")),plot=FALSE)
+ymax <- lims$density |> max()
+hist(-log(1-rbeta(1e6,0.8 * 15, 0.2 * 15)),freq=FALSE,ylim=c(0,ymax+1), border = NA,breaks=100,
+     main = "Prior (grey) vs. posterior (red) for rate of recovery cty 2",
+     xlab = bquote(-log(1-gamma[2])))
+hist(-log(1-fit$draws("gamma[2]")),freq=FALSE,col=rgb(1,0,0,0.5),add=TRUE,border = NA)
+abline(v = -log(1-gamma[2]),col="black")
+
+
+plot(fit$summary("b")$mean, as.vector(beta))
+abline(0,1)
+
+plot(fit$summary("gamma")$mean, as.vector(gamma))
+abline(0,1)
+
+fit$summary("gamma")
+sim_pars <- fit$sampler_diagnostics()
+colSums(sim_pars[,,"treedepth__"] == 14)
+colMeans(sim_pars[,,"treedepth__"])
+
+colMeans(sim_pars[,,"stepsize__"])
+
+meta_data <- fit$metadata()
+names(meta_data)
+meta_data$init
+
+inits <- fit$init()
+summary(inits[[1]]$b)
+summary(inits[[2]]$b)
+summary(inits[[3]]$b)
+summary(inits[[4]]$b)
+
+saveRDS(fit, file = "4-cty-spatial-struct.RDS")
+fit$summary("i0")
+fit$summary("p")
+hist(fit$draws("p"))
+abline(v = 0.6, col = "red")
+fit$summary("b")
+gamma
+plot(fit$summary("gamma")$mean,gamma)
+abline(0,1)
+plot(fit$summary("b")$mean,beta)
+abline(0,1)
+plot(fit$draws("b[1]"),fit$draws("gamma[1]"))
+
+hist(fit$draws("b[1]"))
+abline(v = beta[1],col="red")
+
+hist(fit$draws("b[2]"))
+abline(v = beta[2],col="red")
+
+hist(fit$draws("b[9]"))
+abline(v = beta[9],col="red")
+
+hist(fit$draws("gamma[9]"))
+abline(v = gamma[9],col="red")
+
+dim(fit$draws("gamma[9]"))
+plot(fit$draws("gamma[9]")[,1,],fit$draws("b[9]")[,1,])
+plot(fit$draws("gamma[9]")[,2,],fit$draws("b[9]")[,2,])
+plot(fit$draws("gamma[9]")[,3,],fit$draws("b[9]")[,3,])
+plot(fit$draws("gamma[9]")[,4,],fit$draws("b[9]")[,4,])
+
+
+plot(fit$draws("i0[9]")[,1,],fit$draws("b[9]")[,1,])
+plot(fit$draws("i0[9]")[,1,],fit$draws("b[9]")[,2,])
+plot(fit$draws("i0[9]")[,1,],fit$draws("b[9]")[,3,])
+plot(fit$draws("i0[9]")[,1,],fit$draws("b[9]")[,4,])
+
+np_fit <- nuts_params(fit)
+mcmc_pairs(fit$draws(c("p","i0","gamma","beta")), np = np_fit, pars = c("p","gamma[3]","beta[1,1]","gamma[4]","beta[1,2]"),
+           off_diag_args = list(size = 0.75))
+
+z_t_d <- fit$draws("i_t", format = "draws_array") |> posterior::as_draws_rvars()
+z_t_d <- z_t_d$i_t
+qpt025 <- quantile(z_t_d,0.025)
+qpt975 <- quantile(z_t_d,0.975)
+idx <- 1:2
+ylims <- range(c(
+  range(sweep(qpt975[1,,],MARGIN = 2, STATS = pop_size, FUN = "*")[,idx]),
+  range(sweep(qpt025[1,,],MARGIN = 2, STATS = pop_size, FUN = "*")[,idx])
+))
+matplot((apply(I,2,cumsum) - apply(R,2,cumsum))[,idx], xlab = "Time", 
+     ylab = "Prevalence", main = "Latent prevalence v. time first 2 cties",
+     pch = 19,ylim=ylims)
+matlines(sweep(mean(z_t_d),MARGIN = 2, STATS = pop_size, FUN = "*")[,idx],lty=1)
+matlines(sweep(qpt025[1,,],MARGIN = 2, STATS = pop_size, FUN = "*")[,idx],lty=2)
+matlines(sweep(qpt975[1,,],MARGIN = 2, STATS = pop_size, FUN = "*")[,idx],lty=2)
---
+++ stoch_beta_spatial.stan
@@ -0,0 +1,98 @@
+data {
+  int<lower=1> N_C;
+  int<lower=0> TT;
+  vector<lower=0>[N_C] pop_size;
+  array[TT,N_C] real<lower=0> ii;
+  matrix[N_C, N_C] D;
+}
+parameters {
+
+  matrix[TT, N_C] u_t_logit_eta;
+  matrix[TT, N_C] w_t_logit_eta;
+  row_vector<lower=0, upper=1>[N_C] i0;
+  real<lower=0, upper=1> p;
+  vector<lower=0, upper=1>[N_C] gamma;
+  real<lower=0> sigma; // Standard deviation of noise
+  real<lower=0> rho; // Spatial range parameter
+  real<lower=0> decay_rate_space; // Spatial decay rate
+  vector[N_C] log_beta_diag; // Diagonal elements of log_beta
+}
+
+transformed parameters {
+  matrix[TT, N_C] s_t;
+  matrix[TT, N_C] si_t;
+  matrix[TT, N_C] i_t;
+  matrix[TT, N_C] ir_t;
+  matrix[TT, N_C] u_t;
+  matrix[TT, N_C] w_t;
+  matrix[N_C, N_C] Sigma;
+  matrix[N_C, N_C] log_beta;
+  matrix[N_C, N_C] beta;
+
+   // Create spatial covariance matrix
+  for (i in 1:N_C) {
+    for (j in 1:N_C) {
+      Sigma[i, j] = square(sigma) * exp(-D[i, j] / rho);
+    }
+  }
+
+  // Initialize log_beta with zeros
+  log_beta = rep_matrix(0, N_C, N_C);
+
+  // Set diagonal elements
+  for (i in 1:N_C) {
+    log_beta[i, i] = log_beta_diag[i];
+  }
+
+  // Set off-diagonal elements based on the spatial decay
+  for (i in 1:(N_C - 1)) {
+    for (j in (i + 1):N_C) {
+      //real distance_from_center = abs(i - j);
+      log_beta[i, j] = log_beta[i, i] - decay_rate_space * D[i, j];
+      log_beta[j, i] = log_beta[i, j];
+    }
+  }
+
+  // Exponentiate log_beta to get beta
+  beta = exp(log_beta);
+
+
+  si_t[1,] = rep_row_vector(0,N_C);
+  i_t[1,] = i0;
+  s_t[1] = 1 - i0;
+  ir_t[1,] = rep_row_vector(0,N_C);
+  for (n in 2:TT) {
+    for (ct in 1:N_C) {
+
+      real u_t_mean = exponential_cdf(dot_product(beta[ct,], i_t[n - 1,]) | 1);
+      u_t[n,ct] = inv_logit(u_t_logit_eta[n,ct] / sqrt(pop_size[ct] * s_t[n-1,ct] * u_t_mean * (1 - u_t_mean)) + logit(u_t_mean));
+      w_t[n,ct] = inv_logit(w_t_logit_eta[n,ct] / sqrt(pop_size[ct] * i_t[n-1,ct] * gamma[ct] * (1 - gamma[ct])) + logit(gamma[ct]));
+      si_t[n,ct] = u_t[n,ct] * s_t[n-1,ct];
+      s_t[n,ct] = s_t[n-1,ct] - si_t[n,ct];
+      ir_t[n,ct] = w_t[n,ct] * i_t[n-1,ct];
+      i_t[n,ct] = i_t[n-1,ct] + si_t[n,ct] - ir_t[n,ct];
+    }
+  }
+}
+model {
+  // Priors
+  sigma ~ gamma(2, 2);
+  rho ~ gamma(2, 2);
+  decay_rate_space ~ gamma(4, 4);
+
+  // Prior for log_beta_diag
+  log_beta_diag ~ multi_normal(rep_vector(0, N_C), Sigma);
+
+ i0 ~ beta_proportion(0.01, 50);
+ p ~ beta_proportion(0.75, 5);
+ gamma ~ beta_proportion(0.8, 15);
+ to_vector(u_t_logit_eta) ~ std_normal();
+ to_vector(w_t_logit_eta) ~ std_normal();
+  for (i in 1:TT) {
+    for (ct in 1:N_C) {
+        if (i_t[i,ct] > 0)
+          ii[i,ct] ~ normal(p * pop_size[ct] * i_t[i,ct], sqrt(pop_size[ct] * p * i_t[i,ct] * (1 - p * i_t[i,ct])));
+    }
+  }
+}
+
